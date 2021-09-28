e586540c45130d30f87e6a38645d4bc4050dd347
mikolaj semeniuk <m.semeniuk@icloud.com>
2021-09-28 11:24:27 +0200

    updating sql script

--- init.sql
+++ init.sql
@@ -14,5 +14,18 @@ create table posts (
     user_id int foreign key references users(user_id) on delete cascade
 )
 
+create table user_likes (
+    followed_id INT NOT NULL,
+    follower_id INT NOT NULL,
+    constraint user_like_id primary key (followed_id, follower_id),
+    constraint fk_followed foreign key (followed_id) references users (user_id),
+    constraint fk_follower foreign key (follower_id) references users (user_id)
+)
+
 insert into users (username) values ('john doe'), ('mike mock'), ('lucy applegate'), ('sam taylor')
 insert into posts (title, user_id) values ('lorem', 1), ('ipsum', 1), ('dolor', 2), ('sit', 2), ('amet', 3)
+insert into user_likes (followed_id, follower_id) values (1, 3), (1, 2), (2, 4), (3, 1), (3, 2), (3, 4)
+
+delete from users where user_id = 3
+select * from users
+select * from user_likes
